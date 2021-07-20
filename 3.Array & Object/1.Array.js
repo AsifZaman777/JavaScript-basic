d300acbd40a76fad8c4017a4f7c530b91047b18e
Asif Zaman <69311703+AsifZaman777@users.noreply.github.com>
2021-07-21 01:17:14 +0600

    Add files via upload

---
+++ 3.Array & Object/1.Array.js
@@ -0,0 +1,14 @@
+//array literal//
+const array=["Tamim","Ovy","Jubair","Prottoy",28,true];
+console.log(array);
+
+
+//using new is not a good decision..
+
+var names=new Array("Tamim","Ovy","Jubair","Prottoy",28,true);  //creating instance in the mamory
+ console.log(names);
+
+
+ 
+
+
